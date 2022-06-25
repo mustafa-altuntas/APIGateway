b290a542ea6c438060c9e45964e6a4bbb8b635fc
pc <pc@DESKTOP-FA27JQS>
2022-06-25 23:17:56 +0300

    Add project files.

---
+++ Services/ReservationAPI/Reservation.API/Controllers/ReservationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+
+namespace Reservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly IReservationService reservationService;
+
+        public ReservationController(IReservationService reservationService)
+        {
+            this.reservationService = reservationService;
+        }
+
+        [HttpGet("{bkgNumber}")]
+        public ReservationDTO Get(int bkgNumber)
+        {
+            return reservationService.GetByBkgNumber(bkgNumber);
+        }
+    }
+}
